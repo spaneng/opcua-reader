465ea94e68000b7078455ba50e31fbc984ac71ea
jarrod-span-parallels <jarrod@span-eng.com>
2025-06-20 14:45:33 +1000

    working app

--- src/opcua_reader/app_config.py
+++ src/opcua_reader/app_config.py
@@ -8,21 +8,26 @@ class OpcuaReaderConfig(config.Schema):
         # these 2 are device specific, and inherit from the device-set variables.
         # However, the user can override them if they wish.
 
-        self.num_di = config.Integer(
-            "Digital Input Count",
-            default=config.Variable("device", "digitalInputCount"),
-            minimum=0,
-        )
-        self.num_do = config.Integer(
-            "Digital Output Count",
-            default=config.Variable("device", "digitalOutputCount"),
-            minimum=0,
+        self.ip_address = config.String(
+            "IP Address",
+            description="The IP address of the OPC UA server",
         )
 
-        self.outputs_enabled = config.Boolean("Digital Outputs Enabled", default=True)
-        self.funny_message = config.String("A Funny Message")  # this will be required as no default given.
+        # self.num_di = config.Integer(
+        #     "Digital Input Count",
+        #     default=config.Variable("device", "digitalInputCount"),
+        #     minimum=0,
+        # )
+        # self.num_do = config.Integer(
+        #     "Digital Output Count",
+        #     default=config.Variable("device", "digitalOutputCount"),
+        #     minimum=0,
+        # )
 
-        self.sim_app_key = config.Application("Simulator App Key", description="The app key for the simulator")
+        # self.outputs_enabled = config.Boolean("Digital Outputs Enabled", default=True)
+        # self.funny_message = config.String("A Funny Message")  # this will be required as no default given.
+
+        # self.sim_app_key = config.Application("Simulator App Key", description="The app key for the simulator")
 
 
 if __name__ == "__main__":
