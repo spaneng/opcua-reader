4c3ce480f43373f9d4dbedd0e30db5a31bf5949f
mathsman5133 <mathsman5132@gmail.com>
2025-08-27 13:04:42 +1000

    install deps with uv and change some doover-cli params

--- report_generator/install_deps.bash
+++ report_generator/install_deps.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 set -e
 
@@ -7,12 +7,12 @@ rm -rf include/
 mkdir include/
 touch include/__init__.py
 
-python -m pip install --target=./include/ jinja2
-python -m pip install --target=./include/ weasyprint
-python -m pip install --target=./include/ tzlocal
+uv pip install --target=./include/ jinja2
+uv pip install --target=./include/ weasyprint
+uv pip install --target=./include/ tzlocal
+uv pip install --target=./include/ pydoover
 
-# python -m pip install --target=./include/ pydoover
-python -m pip install --target=./include/ git+https://github.com/spaneng/pydoover.git
+#uv pip install --target=./include/ git+https://github.com/spaneng/pydoover.git
 
 # uv pip install --target=./include/ jinja2
 # uv pip install --target=./include/ weasyprint
--- report_generator/test_report.bash
+++ report_generator/test_report.bash
@@ -7,7 +7,7 @@ AGENT="d8fbeab0-1e72-485e-8f55-a4bee9af0ac4" # OPCUA Re
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/include:$(pwd)"
 
 # Run pydoover as a module instead of trying to use the entry point
-doover compose_report --package_path "report_generator" --agent_ids=$AGENT --agent_names "Test" --enable-traceback --profile solarinjection
+doover report compose --package-path "injector_report" --agent-ids=$AGENT --agent-names "Test" --profile solarinjection
 
 # Option 2: Use the wrapper script (uncomment the line below if you prefer this approach)
 # python include/pydoover_runner.py compose_report \
